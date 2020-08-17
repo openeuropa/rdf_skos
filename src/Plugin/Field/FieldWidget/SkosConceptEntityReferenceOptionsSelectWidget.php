3cabbf87794e749f814ac6ba251448d41b80ae81
Imanol <ier@opendeusto.es>
2020-08-17 15:04:11 +0100

    EWPP-176: Add ordering options to select widget.

--- src/Plugin/Field/FieldWidget/SkosConceptEntityReferenceOptionsSelectWidget.php
+++ src/Plugin/Field/FieldWidget/SkosConceptEntityReferenceOptionsSelectWidget.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 namespace Drupal\rdf_skos\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsSelectWidget;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin for the 'skos_concept_entity_reference_options_select' widget.
@@ -19,4 +21,64 @@ use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsSelectWidget;
  *   multiple_values = TRUE
  * )
  */
-class SkosConceptEntityReferenceOptionsSelectWidget extends OptionsSelectWidget {}
+class SkosConceptEntityReferenceOptionsSelectWidget extends OptionsSelectWidget {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'order' => 'key',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $element['order'] = [
+      '#type' => 'radios',
+      '#title' => t('Select order'),
+      '#default_value' => $this->getSetting('order'),
+      '#options' => [
+        'key' => $this->t('by Key'),
+        'label' => $this->t('by Label'),
+      ],
+      '#description' => t('Select whether the options should be ordered by key or by label.'),
+    ];
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $order = $this->getSetting('order') == 'key' ? 'by Key' : 'by Label';
+    $summary[] = t('Order: @order', ['@order' => $order]);
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getOptions(FieldableEntityInterface $entity) {
+    $options = parent::getOptions($entity);
+    $order = $this->getSetting('order');
+    if ($order == 'key') {
+      return $options;
+    }
+
+    // If we have an empty label take it out before sorting.
+    if ($empty_label = $this->getEmptyLabel()) {
+      array_shift($options);
+    }
+    asort($options);
+    if ($empty_label) {
+      $options = ['_none' => $empty_label] + $options;
+    }
+    return $options;
+  }
+
+}
