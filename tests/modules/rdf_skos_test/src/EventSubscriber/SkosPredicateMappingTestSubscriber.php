d2f1397cc0e4499fc2868c901e7d49617b0ea001
upchuk <danny@webomelette.com>
2020-09-30 12:26:03 +0200

    EWPP-280: Testing the mapping of multiple fields to 1 predicate.

---
+++ tests/modules/rdf_skos_test/src/EventSubscriber/SkosPredicateMappingTestSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\rdf_skos_test\EventSubscriber;
+
+use Drupal\rdf_entity\RdfFieldHandlerInterface;
+use Drupal\rdf_skos\Event\SkosPredicateMappingEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Subscribes to the SKOS predicate mapping event.
+ *
+ * The purpose is to map a single predicate to two separately defined base
+ * fields and ensure that it works.
+ */
+class SkosPredicateMappingTestSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[SkosPredicateMappingEvent::EVENT][] = ['onPredicateMapping', 20];
+    return $events;
+  }
+
+  /**
+   * Maps a predicate to a custom base field on the Skos Concept.
+   *
+   * @param \Drupal\rdf_skos\Event\SkosPredicateMappingEvent $event
+   *   The event.
+   *
+   * @see \oe_content_organisation_entity_base_field_info()
+   */
+  public function onPredicateMapping(SkosPredicateMappingEvent $event): void {
+    $mapping = $event->getMapping();
+    $entity_type_id = $event->getEntityTypeId();
+
+    if ($entity_type_id === 'skos_concept') {
+      $mapping['fields']['dummy_field_one'] = [
+        'column' => 'value',
+        'predicate' => ['http://www.w3.org/2004/02/skos/core#dummy'],
+        'format' => RdfFieldHandlerInterface::TRANSLATABLE_LITERAL,
+      ];
+
+      $mapping['fields']['dummy_field_two'] = [
+        'column' => 'value',
+        'predicate' => ['http://www.w3.org/2004/02/skos/core#dummy'],
+        'format' => RdfFieldHandlerInterface::TRANSLATABLE_LITERAL,
+      ];
+    }
+
+    $event->setMapping($mapping);
+  }
+
+}
